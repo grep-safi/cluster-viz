84d66af679341fa5899f6d10d402301e0a9e209c
grep-safi <hub.of.safi@gmail.com>
2020-06-04 17:20:52 -0700

    streaming data into treemap

--- src/index.js
+++ src/index.js
@@ -1,250 +1,113 @@
-import { data } from "./data";
-import { scaleLinear, scaleOrdinal, format, schemeCategory10, rgb, select, treemap, treemapResquarify, hierarchy } from "d3/dist/d3";
+import { info } from "./data";
+import { create, interpolate, scaleLinear, scaleOrdinal, format, schemeCategory10, rgb, select, treemap, treemapResquarify, hierarchy } from "d3/dist/d3";
+import './flare-2.json';
 
 //https://bl.ocks.org/JacquesJahnichen/42afd0cde7cbf72ecb81
 //https://bl.ocks.org/ganeshv/6a8e9ada3ab7f2d88022
 //https://gist.github.com/tkafka/6d00c44d5ae52182f548a18e8db44811
-var margin = {top: 24, right: 0, bottom: 0, left: 0},
-    width = 1200, //640
-    height = 530,
-    formatNumber = format(",d"),
-    transitioning;
 
-var x = scaleLinear()
-    .domain([0, width])
-    .range([0, width]);
+const width = 954;
+const height = 924;
 
-var y = scaleLinear()
-    .domain([0, height - margin.top - margin.bottom])
-    .range([0, height - margin.top - margin.bottom]);
+const x = scaleLinear().rangeRound([0, width]);
+const y = scaleLinear().rangeRound([0, height]);
+// const data = JSON.stringify(info);
+const data = info;
+console.log(`datazzzzzzz: ${data.children.concat(data)}`);
 
-var color = scaleOrdinal()
-    .range(schemeCategory10
-        .map(function(c) { c = rgb(c); c.opacity = 0.9;  return c; }));
+const divContainer = document.getElementById('domainDrillDown');
 
-var tree;
-var svg, grandparent;
+const svg = create("svg")
+    .attr("viewBox", [0.5, -30.5, width, height + 30])
+    .style("font", "10px sans-serif");
 
-updateDrillDown();
+divContainer.append(svg);
 
-function updateDrillDown() {
-    svg = select("#domainDrillDown").append("svg")
-        .attr("width", width - margin.left - margin.right)
-        .attr("height", height - margin.bottom - margin.top)
-        .style("margin-left", -margin.left + "px")
-        .style("margin.right", -margin.right + "px")
-        // .style("margin.right", -margin.right + "px");
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-        .style("shape-rendering", "crispEdges");
+let group = svg.append("g")
+    .call(render, treemap(data));
 
-    // Root object
-    grandparent = svg.append("g")
-        .attr("class", "grandparent");
+function render(group, root) {
+    // console.log(`root: ${root.children}`);
+    const node = group
+        .selectAll("g")
+        .data(data.children.concat(data))
+        .join("g");
 
-    // Orange rectangle on top
-    grandparent.append("rect")
-        .attr("y", -margin.top)
-        .attr("width", width)
-        .attr("height", margin.top);
+    node.filter(d => d === root ? d.parent : d.children)
+        .attr("cursor", "pointer")
+        .on("click", d => d === root ? zoomout(root) : zoomin(d));
 
-    // Text inside the orange rectangle
-    grandparent.append("text")
-        .attr("x", 6)
-        .attr("y", 6 - margin.top)
-        .attr("dy", ".75em");
+    // node.append("title")
+    //     .text(d => `${d}\n${format(d.value)}`);
 
-    // Actual treemap function
-    tree = treemap()
-        .tile(treemapResquarify.ratio(height / width * 0.5 * (1 + Math.sqrt(5))))
-        .size([width, height])
-        .round(false)
-        .paddingInner(1); // gap between adjacent squares
+    node.append("rect")
+        // .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
+        .attr("fill", d => d === root ? "#fff" : d.children ? "#ccc" : "#ddd")
+        .attr("stroke", "#fff");
 
-    var root = hierarchy(data)
-        .eachBefore(function(d) { d.id = (d.parent ? d.parent.id + "." : "") + d.data.shortName; })
-        .sum((d) => d.size)
-        .sort(function(a, b) {
-            return b.height - a.height || b.value - a.value;
-        });
+    node.append("clipPath")
+        // .attr("id", d => (d.clipUid = DOM.uid("clip")).id)
+        .append("use")
+        // .attr("xlink:href", d => d.leafUid.href);
 
-    initialize(root);
-    accumulate(root);
-    layout(root);
-    tree(root);
-    display(root);
+    node.append("text")
+        // .attr("clip-path", d => d.clipUid)
+        .attr("font-weight", d => d === root ? "bold" : null)
+        .selectAll("tspan")
+        // .data(d => (d === root ? d : d.data.name).split(/(?=[A-Z][^A-Z])/g).concat(format(d.value)))
+        .join("tspan")
+        .attr("x", 3)
+        .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
+        .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
+        .attr("font-weight", (d, i, nodes) => i === nodes.length - 1 ? "normal" : null)
+        .text(d => d);
+
+    group.call(position, root);
 }
 
-function initialize(root) {
-    root.x = root.y = 0;
-    root.x1 = width;
-    root.y1 = height;
-    root.depth = 0;
-}
-
-// Aggregate the values for internal nodes. This is normally done by the
-// treemap layout, but not here because of our custom implementation.
-// We also take a snapshot of the original children (_children) to avoid
-// the children being overwritten when when layout is computed.
-function accumulate(d) {
-    // console.log('accumulate called ' + d.data.name);
-    return (d._children = d.children)
-        ? d.value = d.children.reduce(function(p, v) { return p + accumulate(v); }, 0) : d.value;
-}
-
-// Compute the treemap layout recursively such that each group of siblings
-// uses the same size (1×1) rather than the dimensions of the parent cell.
-// This optimizes the layout for the current zoom state. Note that a wrapper
-// object is created for the parent node for each group of siblings so that
-// the parent’s dimensions are not discarded as we recurse. Since each group
-// of sibling was laid out in 1×1, we must rescale to fit using absolute
-// coordinates. This lets us use a viewport to zoom.
-function layout(d) {
-    if (d._children) {
-        d._children.forEach(function(c) {
-            c.x0 = d.x0 + c.x0 * d.x1;
-            c.y0 = d.y0 + c.y0 * d.y1;
-            c.x1 *= (d.x1 - d.x0);
-            c.y1 *= (d.y1 - d.y0);
-            c.parent = d;
-            layout(c);
-        });
-    }
-}
-
-function display(d) {
-    grandparent
-        .datum(d.parent)
-        .on("click", transition)
-        .select("text")
-        .text(name(d));
-
-    var g1 = svg.insert("g", ".grandparent")
-        .datum(d)
-        .attr("class", "depth");
-
-    var g = g1.selectAll("g")
-        .data(d._children)
-        .enter().append("g");
-
-    g.filter(function(d) { return d._children; })
-        .classed("children", true)
-        .on("click", transition);
-
-    var children = g.selectAll(".child")
-        .data(function(d) { return d._children || [d]; })
-        .enter().append("g");
-
-    children.append("rect")
-        .attr("class", "child")
-        .call(rect)
-        .append("title")
-        .text(function(d) { return d.data.shortName + " (" + formatNumber(d.value) + ")"; });
-
-    children.append("text")
-        .attr("class", "ctext")
-        .text(function(d) { return d.data.shortName; })
-        .call(text2);
-
-    g.append("rect")
-        .attr("class", "parent")
-        .call(rect);
-
-    var t = g.append("text")
-        .attr("class", "ptext")
-        .attr("dy", ".75em")
-
-    t.append("tspan")
-        .text(function(d) { return d.data.shortName; });
-
-    t.append("tspan")
-        .attr("dy", "1.0em")
-        .text(function(d) { return formatNumber(d.value); });
-
-    t.call(text);
-
-    g.selectAll("rect")
-        .style("fill", function(d) { return color(d.data.shortName); });
-
-    function transition(d) {
-        if (transitioning || !d) return;
-        transitioning = true;
-        var g2 = display(d),
-            t1 = g1.transition().duration(750),
-            t2 = g2.transition().duration(750);
-
-        // Update the domain only after entering new elements.
-        //x.domain([d.x0, d.x0 + d.x1]);
-        //y.domain([d.y0, d.y0 + d.y1]);
-        x.domain([d.x0, d.x0 + (d.x1 - d.x0)]);
-        y.domain([d.y0, d.y0 + (d.y1 - d.y0)]);
-
-        // Enable anti-aliasing during the transition.
-        svg.style("shape-rendering", null);
-
-        // Draw child nodes on top of parent nodes.
-        svg.selectAll(".depth").sort(function(a, b) {
-            // console.log('.depth sort a ' + a.depth + ' b ' + b.depth);
-            return a.depth - b.depth; });
-
-        // Fade-in entering text.
-        g2.selectAll("text").style("fill-opacity", 0);
-
-        // Transition to the new view.
-        t1.selectAll(".ptext").call(text).style("fill-opacity", 0);
-        t2.selectAll(".ptext").call(text).style("fill-opacity", 1);
-        t1.selectAll(".ctext").call(text2).style("fill-opacity", 0);
-        t2.selectAll(".ctext").call(text2).style("fill-opacity", 1);
-        t1.selectAll("rect").call(rect);
-        t2.selectAll("rect").call(rect);
-
-        // Remove the old node when the transition is finished.
-        t1.remove().on("end", function() {
-            svg.style("shape-rendering", "crispEdges");
-            transitioning = false;
-        });
-    }
-    return g;
-}
-
-function text(text) {
-    text.selectAll("tspan")
-        .attr("x", function(d) { return x(d.x0) + 6; })
-    text.attr("x", function(d) { return x(d.x0) + 6; })
-        .attr("y", function(d) { return y(d.y0) + 3; })
-        .style("opacity", function(d) {
-            var w = x(d.x1) - x(d.x0);
-            // console.log("text opacity setting textlength " + this.getComputedTextLength() + " d size " + w);
-            return this.getComputedTextLength() < w - 6 ? 1 : 0; });
-}
-
-function text2(text) {
-    text.attr("x", function(d) {
-        return x(d.x1) - this.getComputedTextLength() - 6;
-    })
-        .attr("y", function(d) { return y(d.y1) - 6; })
-        .style("opacity", function(d) {
-            var w = x(d.x1) - x(d.x0);
-            // console.log("text2 opacity setting textlength " + this.getComputedTextLength() + " d size " + w);
-            return this.getComputedTextLength() < w - 6 ? 1 : 0;
-        });
-}
-
-function rect(rect) {
-    rect.attr("x", function(d) { return x(d.x0); })
-        .attr("y", function(d) { return y(d.y0); })
-        .attr("width", function(d) {
-            var w = x(d.x1) - x(d.x0);
-            // console.log('id ' + d.id +' rect width ' + w);
-            return w;
+function position(group, root) {
+    group.selectAll("g")
+        .attr("transform", d => {
+            console.log(`this is ddd: ${JSON.stringify(d)}`);
+            d === root ? `translate(0,-30)` : `translate(${x(d.x0)},${y(d.y0)})`
         })
-        .attr("height", function(d) {
-            var h = y(d.y1) - y(d.y0);
-            // console.log('id ' + d.id +' rect height ' + h);
-            return h;
-        });
+        .select("rect")
+        .attr("width", d => d === root ? width : x(d.x1) - x(d.x0))
+        .attr("height", d => d === root ? 30 : y(d.y1) - y(d.y0));
 }
 
-function name(d) {
-    return d.parent ? name(d.parent) + " / " + d.data.shortName + " (" + formatNumber(d.value) + ")" : d.data.shortName + " (" + formatNumber(d.value) + ")";
+// When zooming in, draw the new nodes on top, and fade them in.
+function zoomin(d) {
+    const group0 = group.attr("pointer-events", "none");
+    const group1 = group = svg.append("g").call(render, d);
+
+    x.domain([d.x0, d.x1]);
+    y.domain([d.y0, d.y1]);
+
+    svg.transition()
+        .duration(750)
+        .call(t => group0.transition(t).remove()
+            .call(position, d.parent))
+        .call(t => group1.transition(t)
+            .attrTween("opacity", () => interpolate(0, 1))
+            .call(position, d));
 }
+
+// When zooming out, draw the old nodes on top, and fade them out.
+function zoomout(d) {
+    const group0 = group.attr("pointer-events", "none");
+    const group1 = group = svg.insert("g", "*").call(render, d.parent);
+
+    x.domain([d.parent.x0, d.parent.x1]);
+    y.domain([d.parent.y0, d.parent.y1]);
+
+    svg.transition()
+        .duration(750)
+        .call(t => group0.transition(t).remove()
+            .attrTween("opacity", () => interpolate(1, 0))
+            .call(position, d))
+        .call(t => group1.transition(t)
+            .call(position, d.parent));
+}
+
+// return svg.node();
