e12a813f3e87836b4bd0d3cf2c93d693d5586833
grep-safi <hub.of.safi@gmail.com>
2020-06-16 00:52:03 -0700

    FormatSControlData works correctly now

--- data/format.js
+++ data/format.js
@@ -1,34 +1,12 @@
-const fs = require('fs')
+const str = 'NodeName=nid00040 Arch=x86_64 CoresPerSocket=16  CPUAlloc=0 CPUTot=64 CPULoad=0.00 AvailableFeatures=haswell ' +
+    'ActiveFeatures=haswell Gres=craynetwork:4,preemptable:1 NodeAddr=nid00040 NodeHostName=nid00040 Version=19.05.6 i' +
+    'OS=Linux 4.12.14-150.17_5.0.90-cray_ari_c #1 SMP Tue Apr 28 21:17:03 UTC 2020 (3e6e478)  RealMemory=120832 AllocMem=0 ' +
+    'FreeMem=120750 Sockets=2 Boards=1 State=DOWN ThreadsPerCore=2 TmpDisk=0 Weight=1000 Owner=N/A MCS_label=N/A ' +
+    'Partitions=system,resv,resv_shared,interactive  BootTime=2020-05-31T21:02:20 SlurmdStartTime=2020-05-31T21:22:37 ' +
+    'CfgTRES=cpu=64,mem=118G,billing=64 AllocTRES= CapWatts=n/a CurrentWatts=125 AveWatts=0 ExtSensorsJoules=n/s ' +
+    'ExtSensorsWatts=0 ExtSensorsTemp=n/s Reason=repair:cray:INC0155505 [root@2020-05-27T04:24:37]';
 
-fs.readFile('data-files/cluster-data.txt', (err, data) => {
-    if (err) throw err;
-
-    const dataStr = data.toString();
-    const dataArr = dataStr.split('\n');
-    const formattedDataArr = [];
-    formattedDataArr.push('[\n')
-
-    for (let i = 0; i < dataArr.length - 1; i++) {
-        const str = dataArr[i];
-        const comma = i === dataArr.length - 2 ? '' : ',';
-
-        const dataLine = formatData(str, comma);
-        formattedDataArr.push(`\t${dataLine}\n`);
-    }
-
-    formattedDataArr.push(']');
-    const formattedDataStr = formattedDataArr.join(' ');
-
-    let jsonStr = JSON.parse(formattedDataStr);
-
-    fs.writeFile('formattedSControl.txt', formattedDataStr, function(err) {
-        if (err) {
-            return console.error(err);
-        }
-
-        console.log(`Data formatted successfully and stored in formattedSControl.txt file!`);
-    })
-});
+formatData(str, '');
 
 function formatData(string, comma) {
     let modifiedStr = string.replace(/  /g, ' ');
@@ -53,6 +31,8 @@ function formatData(string, comma) {
         endExclusion = len + 1;
     }
 
+    console.log(`start and end: ${startExclusion} and ${endExclusion}`);
+
     for (let i = 0; i < len; i++) {
         // We don't want to include the OS= field in our transformations
         if (!(i >= startExclusion && i < endExclusion)) {
@@ -75,6 +55,8 @@ function formatData(string, comma) {
     // Parse with json
     // let jsonStr = JSON.parse(modifiedStr);
 
+    console.log(`modified str: ${modifiedStr}`);
+
     return modifiedStr;
 }
 
@@ -92,13 +74,15 @@ function indexOfKey(arr, targetStr) {
 function handleRepairField(arr) {
     let index = indexOfKey(arr, 'Reason');
     if (index !== -1) {
+        console.log(`index: ${index} and length: ${arr.length}`);
         let reasonMessage = "";
         let len = arr.length;
         for (let i = index; i < len - 1; i++) {
             let str = arr.pop();
+            console.log(`strr:: ${str}\n\n`);
             reasonMessage = reasonMessage.concat(` ${str}`);
         }
-
+        console.log(`rsnmsg: ${reasonMessage} arr: ${arr.toString()}`);
         arr[index] = arr[index].concat(`${reasonMessage}`);
     }
 }
