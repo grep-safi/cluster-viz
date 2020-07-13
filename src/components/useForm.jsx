d73b330bbe3acb4a8492191f07c60e095d01b07e
grep-safi <hub.of.safi@gmail.com>
2020-07-13 14:46:43 -0700

    Built website and added documentation for the useForm function

--- src/components/useForm.jsx
+++ src/components/useForm.jsx
@@ -10,7 +10,7 @@ export default initialValues => {
      * @param i            {Number}         - Index of the element in the array to be modified or deleted
      * @param addSearch    {boolean}        - Tells function to add a new object to the array
      * @param removeSearch {boolean}        - Tells function to remove a specified element in the array
-     *
+     * @param refresh      {function}       - Updates the counter so that the page refreshes and reflects new data
      */
     const handleChanges = (e, i, addSearch, removeSearch, refresh) => {
         // React's setValue will only trigger a re-render if it detects
